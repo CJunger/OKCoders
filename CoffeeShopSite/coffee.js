4745e402146f4256095bd31c8090a80a3070808f
Chelsea <131617662+CJunger@users.noreply.github.com>
2024-03-16 12:45:18 -0500

    coffee shop login button, Module 2 Day 4 work

---
+++ CoffeeShopSite/coffee.js
@@ -0,0 +1,28 @@
+let userName;
+let unArr;
+let avatar;
+const loginbutton = document.getElementById('login');
+
+loginbutton.onclick = loginUser
+
+function loginUser () {
+        userName = prompt('Please enter First and Last Name');
+        avatar = createAvatar(userName);
+        displayAvatar();
+}
+function createAvatar (userName) {
+    if (!userName) {
+        alert ('We need a name!');
+        return;
+    }
+    unArr = userName.split(' ');
+    if (unArr.length <2) {
+        alert('You must enter two names');
+        return;
+    }
+    return unArr[0][0] + unArr [1][0];
+}
+function displayAvatar() {
+    loginbutton.style.display = 'none';
+    document.getElementById('avatar').innerText = avatar;
+}
